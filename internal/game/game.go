c3ac932229c903eb5904f5023af4ed0ed1588b3a
Brij Kalakheti <kalakheti_@outlook.com>
2025-02-21 14:17:31 -0500

    define engine struct and method setwalls

--- internal/game/game.go
+++ internal/game/game.go
@@ -4,7 +4,25 @@ import (
 	"github.com/rkalaa/go-engine/internal/objects"
 )
 
-func StartOppositeObjects(posX, posY float64, speedX, speedY float64, Width, Height int) [2]objects.Object {
+type Engine struct {
+	Objects [2]objects.Object
+	Walls   [4]objects.Object
+}
+
+func (e *Engine) SetWalls(screenWidth, screenHeight int) {
+	floatscreenWidth, floatscreenHeight := float64(screenWidth), float64(screenHeight)
+	iteratex := [4]float64{floatscreenWidth / 2, 0, floatscreenWidth, floatscreenWidth / 2}
+	iteratey := [4]float64{0, floatscreenHeight / 2, floatscreenHeight / 2, floatscreenHeight}
+	for i := 0; i < len(iteratex) && i < len(iteratey); i++ {
+		WallPosition := objects.Vector{XValue: iteratex[i], YValue: iteratey[i]}
+		WallVelocity := objects.Vector{XValue: 0, YValue: 0}
+		Wall := objects.Object{Position: WallPosition, Velocity: WallVelocity, Width: int(iteratex[i]), Height: int(iteratey[i])}
+		e.Walls[i] = Wall
+	}
+
+}
+
+func (e *Engine) StartOppositeObjects(posX, posY float64, speedX, speedY float64, Width, Height int) {
 	//first object assigned
 	Position := objects.Vector{XValue: posX, YValue: posY}
 	Velocity := objects.Vector{XValue: speedX, YValue: posY}
@@ -15,17 +33,17 @@ func StartOppositeObjects(posX, posY float64, speedX, speedY float64, Width, Hei
 	Object2 := objects.Object{Position: InvertPosition, Velocity: InvertVelocity, Width: Width, Height: Height}
 
 	AllObjects := [2]objects.Object{Object1, Object2}
-	return AllObjects
+	e.Objects = AllObjects
 }
 
-func CheckCollisions(AllObjects [2]objects.Object) bool {
-	/* Checks if x, only then will compute y to optimize
+func (e *Engine) CheckCollisions() bool {
+	/* Checks if x, only then will compute y to optimize AABB methodology
 	good: (1L)(1R)(2L)(2R)
 	bad: (1L)(2L)(1R)(2R)
 	if bad check y
 	*/
-	Object1, Object2 := AllObjects[0], AllObjects[1]
-	Position1, Position2 := AllObjects[0].Position, AllObjects[1].Position
+	Object1, Object2 := e.Objects[0], e.Objects[1]
+	Position1, Position2 := e.Objects[0].Position, e.Objects[1].Position
 
 	Object1LX := Position1.XValue - float64(Object1.Width)
 	Object1LeftVector := objects.Vector{XValue: Object1LX, YValue: 0}
@@ -46,8 +64,8 @@ func CheckCollisions(AllObjects [2]objects.Object) bool {
 	return false
 }
 
-func IncrementPosition(AllObjects *[2]objects.Object) {
-	(*AllObjects)[0].Position = (*AllObjects)[0].Position.Add((*AllObjects)[0].Velocity)
-	(*AllObjects)[1].Position = (*AllObjects)[0].Position.Add((*AllObjects)[1].Velocity)
+func (e *Engine) IncrementPosition() {
+	(e.Objects)[0].Position = (e.Objects)[0].Position.Add((e.Objects)[0].Velocity)
+	(e.Objects)[1].Position = (e.Objects)[0].Position.Add((e.Objects)[1].Velocity)
 
 }
