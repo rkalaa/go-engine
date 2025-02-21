c8c6a4f41eb69743ed77398be6053bace22f5952
Brij Kalakheti <kalakheti_@outlook.com>
2025-02-21 10:49:19 -0500

    collisions and position update completed

--- internal/game/game.go
+++ internal/game/game.go
@@ -18,7 +18,7 @@ func StartOppositeObjects(posX, posY float64, speedX, speedY float64, Width, Hei
 	return AllObjects
 }
 
-func CheckCollisions(AllObjects [2]objects.Object) {
+func CheckCollisions(AllObjects [2]objects.Object) bool {
 	/* Checks if x, only then will compute y to optimize
 	good: (1L)(1R)(2L)(2R)
 	bad: (1L)(2L)(1R)(2R)
@@ -26,9 +26,28 @@ func CheckCollisions(AllObjects [2]objects.Object) {
 	*/
 	Object1, Object2 := AllObjects[0], AllObjects[1]
 	Position1, Position2 := AllObjects[0].Position, AllObjects[1].Position
-	Object1LX, Object1RX := Position1.XValue-float64(Object1.Width), Position1.XValue+float64(Object1.Width)
-	Object2LX, Object2RX := Position2.XValue-float64(Object2.Width), Position2.XValue+float64(Object2.Width)
 
+	Object1LX := Position1.XValue - float64(Object1.Width)
+	Object1LeftVector := objects.Vector{XValue: Object1LX, YValue: 0}
+
+	Object2LX := Position2.XValue - float64(Object2.Width)
+	Object2LeftVector := objects.Vector{XValue: Object2LX, YValue: 0}
+
+	if float64(Object1.Width) > Object1LeftVector.Distance(Object2LeftVector) {
+		Object1UY := Position1.YValue + float64(Object1.Height)
+		Object1UpVector := objects.Vector{XValue: 0, YValue: Object1UY}
+
+		Object2UY := Position2.YValue + float64(Object2.Height)
+		Object2UpVector := objects.Vector{XValue: 0, YValue: Object2UY}
+		if float64(Object1.Height) > Object1UpVector.Distance(Object2UpVector) {
+			return true
+		}
+	}
+	return false
 }
 
-func IncrementPosition()
+func IncrementPosition(AllObjects *[2]objects.Object) {
+	(*AllObjects)[0].Position = (*AllObjects)[0].Position.Add((*AllObjects)[0].Velocity)
+	(*AllObjects)[1].Position = (*AllObjects)[0].Position.Add((*AllObjects)[1].Velocity)
+
+}
